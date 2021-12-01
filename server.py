75fceff634c227ad98ada05b190532399df1b71b
amirmahdi <amirmahdichiti123@gmail.com>
2021-12-01 13:01:09 +0330

    add critical to uvicorn

--- server.py
+++ server.py
@@ -7,10 +7,11 @@ for f in client.friend_nodes:
     print(f)
 
 app = FastAPI()
+
+
 def run_server():
     if __name__ == "__main__":
-        uvicorn.run(app, host="localhost", port=client.port_number, access_log=False)
-
+        uvicorn.run(app, host="localhost", port=client.port_number, access_log=False, log_level="critical")
 
 
 def read_request():
@@ -26,12 +27,13 @@ def read_request():
 t1 = threading.Thread(target=run_server)
 t2 = threading.Thread(target=read_request)
 
-
 t1.start()
 t2.start()
 
 t1.join()
 t2.join()
-#read_request()
-#@app.get("/file")
-#async def getfile(filename):
+
+
+@app.get("/file")
+def getfile(filename: str):
+    return filename
