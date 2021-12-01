2b4d6ad01395edcd2091951e92fd0fb3f9f4337c
amirmahdi <amirmahdichiti123@gmail.com>
2021-12-01 12:51:18 +0330

    change server file

--- server.py
+++ server.py
@@ -11,31 +11,27 @@ def run_server():
     if __name__ == "__main__":
         uvicorn.run(app, host="localhost", port=client.port_number, access_log=False)
 
-while(True):
-    input = input()
-    print(input)
+
 
 def read_request():
-    #global input
-    #input:str
-   # while (True):
-        input = input()
-        x = input.split()
+    while (True):
+        input_str = input()
+        x = input_str.split()
         if x[0] == "request":
             print("request")
         else:
             print("command not found")
 
 
-#t1 = threading.Thread(target=run_server)
-#t2 = threading.Thread(target=read_request)
+t1 = threading.Thread(target=run_server)
+t2 = threading.Thread(target=read_request)
 
 
-#t1.start()
-#t2.start()
+t1.start()
+t2.start()
 
-#t1.join()
-#t2.join()
+t1.join()
+t2.join()
 #read_request()
 #@app.get("/file")
 #async def getfile(filename):
