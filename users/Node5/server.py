432d8bda01e16d60deb2d9f7d671138a063d74e4
majiddrn <dareinimajid34@gmail.com>
2021-12-03 15:24:32 +0330

    server.py changed - completed

--- users/Node5/server.py
+++ users/Node5/server.py
@@ -9,6 +9,9 @@ from fastapi.responses import FileResponse
 
 # for f in util.friend_nodes:
 #     print(f)
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s',filename='../Network.log', encoding='utf-8', level=logging.INFO)
+
 
 app = FastAPI()
 
@@ -37,7 +40,6 @@ def getfile(file_name: str, seen: str):
         if ownedFile == file_name:
             # return FileResponse(path=f"./{util.owned_files_dir}/{file_name}",
             #                     media_type="text", status_code=200)
-            print(type(util.port_number))
             seenNew = seen + "," + str(util.node_number)
             return (f'{str(util.port_number)}|{seenNew}')
 
@@ -53,7 +55,6 @@ def getfile(file_name: str, seen: str):
         return (f'-1|{seenNew}')
 
     for ownFriend in friendNodes:
-        print(f'http://localhost:{ownFriend["node_port"]}/port')
 
         if checkSeen(ownFriend["node_name"], seenNodes):
             continue
@@ -65,9 +66,7 @@ def getfile(file_name: str, seen: str):
         # if f.content.decode('ascii') != "-1":
         #     break
         f = f.text[1:(len(f.text) - 1)]
-        print(f)
         if f.split("|")[0] != '-1':
-            print("break")
             break
 
     return f
@@ -79,7 +78,7 @@ def getfile(file_name: str, seen: str):
 
 def run_server():
     if __name__ == "__main__":
-        uvicorn.run(app, host="localhost", port=util.port_number, access_log=False)
+        uvicorn.run(app, host="localhost", port=util.port_number, access_log=False,log_level="critical")
 
 
 def read_request():
@@ -88,9 +87,10 @@ def read_request():
         x = input_str.split()
 
         if x[0] == "request":
-            print("request")
             fileRequested = x[1]
+            logging.info(f"{util.node_number} requested {fileRequested}")
         else:
+            logging.warning(f"Node{util.node_number} wrong request")
             print("command not found")
             continue
 
@@ -99,7 +99,7 @@ def read_request():
             if (ownFile == fileRequested):
                 fileFound = True
                 print(f"GET FILE FROM NODE {util.node_number}")
-
+                logging.info(f" node {util.node_number} got {fileRequested} from port {util.port_number}")
         if not fileFound:
             for ownFriends in util.friend_nodes:
                 port = requests.get(f"http://localhost:{ownFriends['node_port']}/port",
@@ -110,16 +110,19 @@ def read_request():
                 #     break
                 port = port.text[1:(len(port.text) - 1)]
                 if port.split("|")[0] != '-1':
-                    print(f"A file found {port}")
                     break
 
-            print(port)
-            f = requests.get(f"http://localhost:{port.split('|')[0]}/file",
-                             params={"file_name": fileRequested})
-            fw = open(f"./{util.new_files_dir}/{fileRequested}", "wb")
-            fw.write(f.content)
-            fw.close()
-
+            if port.split("|")[0] != '-1':
+                f = requests.get(f"http://localhost:{port.split('|')[0]}/file",
+                                 params={"file_name": fileRequested})
+                fw = open(f"./{util.new_files_dir}/{fileRequested}", "wb")
+                fw.write(f.content)
+                fw.close()
+                print(f"{util.node_number} got {fileRequested} from port {port.split('|')[0]}")
+                logging.info(f"{util.node_number} got {fileRequested} from port {port.split('|')[0]}")
+            else:
+                print(f"couldn't get {fileRequested}")
+                logging.warning(f"couldn't get {fileRequested}")
 
 t1 = threading.Thread(target=run_server)
 t2 = threading.Thread(target=read_request)
